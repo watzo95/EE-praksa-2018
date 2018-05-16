3f89806239eec37646bf8ea77cbbf997a3c5588f
j4nb3 <jan.bec@student.um.si>
2018-05-16 23:59:27 +0200

    fix

--- API/api/views/features_views.py
+++ API/api/views/features_views.py
@@ -4,5 +4,6 @@ from api.models import Feature, Template, Name
 from rest_framework.decorators import api_view
 
 class GetFeatures(generics.ListAPIView):
-    queryset = Feature.objects.filter(feature__template__template_name='coinbase')
+    #queryset = Feature.objects.filter(feature__template__template_name='coinbase')
+    queryset = Feature.objects.filter()
     serializer_class = FeaturesSerializer
